9d5bd36b4dda74801a28a742fabc31305fa7ec05
Alex Tkachev <tkachev.alex@gmail.com>
2011-07-18 15:00:52 +0300

    first version

---
+++ dist/jquery.i18n.pack.js
@@ -0,0 +1,13 @@
+/*
+* jqI18n - jQuery plugin for rendering dialog
+*
+* Version: 0.0.1a
+* Copyright 2011 Alex Tkachev
+*
+* Dual licensed under MIT or GPLv2 licenses
+*   http://en.wikipedia.org/wiki/MIT_License
+*   http://en.wikipedia.org/wiki/GNU_General_Public_License
+*
+* Date: Sun Jul 17 19:48:23 2011 +0300
+*/
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--){d[e(c)]=k[c]||e(c)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('(5(a){4 b=5(){3.v.Q(3,9)};a.M(b.p,{v:5(){3.m="B-C",3.l={},3.l[3.m]={}},F:5(a){6(9.8===0)7 3.m;6(!3.l[a])L"G "+a+" H I J";3.m=a;7 3},s:5(b,c){4 d=3.l[3.m],e=b.K("."),f=d;o(4 g=0;g<e.8-1;g++){f=f[e[g]];6(a.u(f)!="x")7 b}f=f[e[e.8-1]];6(N f=="O")7 b;6(9.8==2&&a.u(c)=="x"){o(4 h y c)6(c.z(h)){4 i=n q("%\\\\{"+h+"\\\\}","w");f=f.r(i,c[h])}}D 6(9.8>1)o(4 j=1;j<9.8;j++){4 k=n q("\\\\{"+(j-1)+"\\\\}","w");f=f.r(k,9[j])}7 f},E:5(a,b){3.l[a]=b}}),b.p.t=b.p.s,a.A=5(){7 n b}})(P)',53,53,'|||this|var|function|if|return|length|arguments||||||||||||_regional|_locale|new|for|prototype|RegExp|replace|translate||type|initialize|gm|object|in|hasOwnProperty|i18n|en|US|else|register|locale|Locale|is|not|registered|split|throw|extend|typeof|undefined|jQuery|apply'.split('|'),0,{}))
---
+++ test/unit/i18n-test.js
@@ -0,0 +1,70 @@
+module("I18n", {setup: function(){
+  var obj = this.obj = function(){
+    this.age = 111;
+  };
+  obj.i18n = $.i18n();
+
+  obj.i18n.register('en-US', {
+    age: '1234',
+    name: 'alex',
+    messages: {
+      intro: 'welcome',
+      error: 'error code is %{code}',
+      notice: 'number %{n} is greater than %{l} because its %{n}',
+      yoko: {
+        koko: 'asdf',
+        moko: 'asdfasdf'
+      }
+    },
+    numbered: "one is {0} and two is {1} and again one is {0}"
+  });
+
+  obj.i18n.register('de-GR', {
+    age: '4321',
+    name: 'asdf',
+    messages: {
+      intro: 'welcome',
+      error: 'error code is %{code}',
+      notice: 'number %{n} is greater than %{l} because its %{n}'
+    },
+    numbered: "one is {0} and two is {1} and again one is {0}"
+  });
+
+}});
+
+test("Basic requirements", function() {
+  ok( $.i18n, "$.i18n not present" );
+});
+
+test("locale()", function() {
+  var obj = this.obj;
+
+  equals(obj.i18n.locale(), 'en-US');
+  equals(obj.i18n.translate('puki'), 'puki');
+  equals(obj.i18n.translate('puki.do'), 'puki.do');
+  equals(obj.i18n.t('puki'), 'puki');
+  equals(obj.i18n.t('puki.do'), 'puki.do');
+  raises(function(){ obj.i18n.locale('he-IL')} , "Locale he-IL is not registered");
+
+  obj.i18n.locale('de-GR');
+  equals(obj.i18n.locale(), 'de-GR');
+});
+
+test("translate()", function() {
+  var obj = this.obj;
+
+  equals(obj.i18n.locale(), 'en-US');
+  equals(obj.i18n.translate('age'), '1234');
+  equals(obj.i18n.t('age'), '1234');
+  equals(obj.i18n.t('name'), 'alex');
+  equals(obj.i18n.t('messages.intro'), 'welcome');
+  equals(obj.i18n.t('messages.yoko.koko'), 'asdf');
+  equals(obj.i18n.t('messages.yoko.moko'), 'asdfasdf');
+  equals(obj.i18n.t('messages.error'), 'error code is %{code}');
+  equals(obj.i18n.t('messages.error', {code: 404}), 'error code is 404');
+  equals(obj.i18n.t('messages.error', {puki: 404}), 'error code is %{code}');
+  equals(obj.i18n.t('messages.notice', {l: 4, n: 5}), 'number 5 is greater than 4 because its 5');
+  equals(obj.i18n.t('numbered', 11, 22), 'one is 11 and two is 22 and again one is 11');
+
+
+});
